bfb56a0e99f20be711d80b43160922c50334c2a9
Amandeep Singh <amandees@thoughtworks.com>
2015-09-19 12:43:38 +0530

    Added the functionality in willDieOfLoneliness to return me false when i call it undying cell coordinates and added spec for that too

--- test/com.thoughtworks.main/GridTest.java
+++ test/com.thoughtworks.main/GridTest.java
@@ -2,6 +2,9 @@ package com.thoughtworks.main;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class GridTest {
@@ -22,5 +25,15 @@ public class GridTest {
         grid.initializeGridCells(4, 4);
 
         assertEquals(false, grid.willDieOfLoneliness(-1, -3));
+    }
+
+    @Test
+    public void shouldReturnMeFalseWhenICallShouldDieOfLonelinessMethodWithCellIsNotValidDying() {
+        grid = new Grid();
+        grid.initializeGridCells(2, 2);
+        List<String> list = Arrays.asList("x x", "x x");
+        grid.populateCells(list);
+
+        assertEquals(false, grid.willDieOfLoneliness(1, 1));
     }
 }
