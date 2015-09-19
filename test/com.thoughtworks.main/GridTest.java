ec13eca0ff4fcb4aa66d58fb2490a4c633228c6f
Amandeep Singh <amandees@thoughtworks.com>
2015-09-19 13:23:33 +0530

    Added the functionality in willDieOfOverCrowding to return me false when i call it with not valid dying cell and added spec for that too

--- test/com.thoughtworks.main/GridTest.java
+++ test/com.thoughtworks.main/GridTest.java
@@ -46,4 +46,14 @@ public class GridTest {
 
         assertEquals(false, grid.willDieOfOverCrowding(-1, -3));
     }
+
+    @Test
+    public void shouldReturnMeFalseWhenICallWillDieOfOverCrowdingMethodWithCellIsNotValidDying() {
+        grid = new Grid();
+        grid.initializeGridCells(2, 2);
+        List<String> list = Arrays.asList("x x", "x x");
+        grid.populateCells(list);
+
+        assertEquals(false, grid.willDieOfOverCrowding(1, 1));
+    }
 }
